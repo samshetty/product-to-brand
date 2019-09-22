0de42fbc9697dd8489058323ef6a3434b3db7ab6
samshetty <sampatshetty@gmail.com>
2019-09-21 22:25:59 -0400

    Add files via upload

---
+++ product_to_brand.py
@@ -0,0 +1,27 @@
+from sentence_transformers import SentenceTransformer
+import scipy.spatial
+
+embedder = SentenceTransformer('bert-base-nli-mean-tokens')
+corpus = ['apple inc.', 'microsoft Corporation', 'honda', 'samsung electronics',  
+          'bloomberg llc', 'toyota', 'google', 'facebook', 'pfizer', 'amazon']
+corpus_embeddings = embedder.encode(corpus)
+
+queries = ['ipad pro', 'iphone 10 pro', 'kindle e-reader', 'android', 'accord ex', 'windows os',
+           'camry le', 'galaxy note phone', 'galaxy note', 'civic'
+           ,'pixel phone', 'xbox one', 'fire tv']
+query_embeddings = embedder.encode(queries)
+
+closest_n = 1
+for query, query_embedding in zip(queries, query_embeddings):
+    distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+
+    results = zip(range(len(distances)), distances)
+    results = sorted(results, key=lambda x: x[1])
+    
+    print("======================")
+    print("Query:", query)
+    print("Result:")
+
+    for idx, distance in results[0:closest_n]:
+        result = corpus[idx].strip() + "(Score: %.4f)" % (1-distance) if 1-distance > 0.6 else 'Match not found.'
+        print(result)
