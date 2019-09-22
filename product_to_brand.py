bc4ba13578fa612656dcdd5b39440edbf69bde03
samshetty <sampatshetty@gmail.com>
2019-09-21 23:12:52 -0400

    Update product_to_brand.py

--- product_to_brand.py
+++ product_to_brand.py
@@ -3,11 +3,12 @@ import scipy.spatial
 
 embedder = SentenceTransformer('bert-base-nli-mean-tokens')
 corpus = ['apple inc.', 'microsoft Corporation', 'honda', 'samsung electronics',  
-          'bloomberg llc', 'toyota', 'google', 'facebook', 'pfizer', 'amazon']
+          'bloomberg llc', 'toyota', 'google', 'facebook', 'amazon']
 corpus_embeddings = embedder.encode(corpus)
 
-queries = ['ipad pro', 'iphone 10 pro', 'kindle e-reader', 'android', 'accord ex', 'windows os',
-           'camry le', 'galaxy note phone', 'galaxy note', 'civic'
+queries = ['ipad pro', 'iphone 10 plus','iphone', 'kindle e-reader', 'android', 'accord ex', 'windows os', 'windows',
+            'windows operating system',
+           'camry le', 'galaxy s9', 'galaxy s8', 'civic', 'chromecast'
            ,'pixel phone', 'xbox one', 'fire tv']
 query_embeddings = embedder.encode(queries)
 
@@ -25,3 +26,4 @@ for query, query_embedding in zip(queries, query_embeddings):
     for idx, distance in results[0:closest_n]:
         result = corpus[idx].strip() + "(Score: %.4f)" % (1-distance) if 1-distance > 0.6 else 'Match not found.'
         print(result)
+        
